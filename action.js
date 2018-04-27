63c7032d4ee9121fec5175bbec7ee3627f48f2c0
Cody Gates <cody.gates.89@gmail.com>
2018-04-26 23:58:53 -0600

    implement styling to page

--- action.js
+++ action.js
@@ -1,8 +1,9 @@
 
 let selCourse;
 let selTee;
-let allcourses;
+let allCourses;
 let numPlayers = 4;
+let right = $(".right");
 
 loadDoc();
 
@@ -10,10 +11,9 @@ function loadDoc() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            allcourses = JSON.parse(this.responseText);
-            console.log(allcourses);
-            for(let i = 0; i < allcourses.courses.length; i++) {
-                $(".courseDropdown").append("<option value='"+ allcourses.courses[i].id +"'>" + allcourses.courses[i].name + "</option>");
+            allCourses = JSON.parse(this.responseText);
+            for(let i = 0; i < allCourses.courses.length; i++) {
+                $(".courseDropDown").append("<option value='"+ allCourses.courses[i].id +"'>" + allCourses.courses[i].name + "</option>");
             }
         }
     };
@@ -21,54 +21,118 @@ function loadDoc() {
     xhttp.send();
 }
 
-function getCourse(courseid) {
-    console.log(courseid);
-    $(".teeDropdown").html('');
+function chooseCourse(courseId) {
+    $(".chooseTee").html('');
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             selCourse = JSON.parse(this.responseText);
-            console.log(selCourse);
             let holeonetees = selCourse.data.holes[0].teeBoxes;
             for(let i = 0; i < holeonetees.length; i++){
-                $(".teeDropdown").append("<option value='"+ i +"'>"+ holeonetees[i].teeType +"</option>");
+                $(".teeDropDown").append("<option value='"+ i +"'>"+ holeonetees[i].teeType +"</option>");
             }
         }
     };
-    xhttp.open("GET", "https://uxcobra.com/golfapi/course"+ courseid +".txt", true);
+    xhttp.open("GET", "https://uxcobra.com/golfapi/course"+ courseId +".txt", true);
     xhttp.send();
 }
 
 function setTee(teeindex){
-    $(".right").html('');
+    $(right).html('');
+    $(right).append(
+        "<div class='column' id='colLabel'>"+
+        "<div class='colHead box'>" + "Hole #" + "</div>"+
+        "<div class='yds box'>" + "Yards" + "</div>"+
+        "<div class='par box'>" + "Par" + "</div>"+
+        "<div class='hcp box'>" + "Hcp" + "</div>"+
+        "</div>"
+    );
+
     selTee = teeindex;
+
+    let frontTotalYds = 0;
+    let frontTotalPar = 0;
+    let backTotalYds = 0;
+    let backTotalPar = 0;
+    let totalYds = 0;
+    let totalPar = 0;
+    let totalScore = 0;
+
+
     let mycourse = selCourse.data.holes;
     for(let i = 0; i < mycourse.length; i++){
-        $(".right").append("<div class='column' id='c"+ 1 +"'>"+
-            "<div class='cheader'>"+ (i+1) + "</div>"+
-            "<div class='yds'>"+ mycourse[i].teeBoxes[teeindex].yards +"</div>"+
+        $(right).append(
+            "<div class='column' id='c"+ i +"'>"+
+            "<div class='colHead box'>"+ (i+1) + "</div>"+
+            "<div class='yds box'>"+ mycourse[i].teeBoxes[teeindex].yards +"</div>"+
+            "<div class='par box'>"+ mycourse[i].teeBoxes[teeindex].par +"</div>"+
+            "<div class='hcp box'>"+ mycourse[i].teeBoxes[teeindex].hcp +"</div>"+
             "</div>");
         }
-    fillCard();
+
+        $(right).append(
+            "<div class='column' id='colLabel'>"+
+            "<div class='colHead box'>In</div>"+
+            "<div class='yds totalIn box' id='backNineTotalYds'>" + backTotalYds + "</div>"+
+            "<div class='par totalIn box' id='backNineTotalPar'>" + backTotalPar + "</div>"+
+            "<div class='hcp box'>" + '' +"</div>"+
+            "</div>"+
+            "<div class='column' id='totalCol'>"+
+            "<div class='colHead box'>" + "Total" + "</div>"+
+            "<div class='yds box'>" + totalYds + "</div>"+
+            "<div class='par box'>" + totalPar + "</div>"+
+            "<div class='hcp box'>" + '' +"</div>"+
+            //"<div class='total box'>" + '' +"</div>"+
+            "</div>"
+        );
+
+
+        $("#c8").after(
+            "<div class='column out'>"+
+            "<div class='colHead box'>Out</div>"+
+            "<div class='yds box' id='frontNineYdsTot'>" + frontTotalYds + "</div>"+
+            "<div class='par box' id='frontNineParTot'>" + frontTotalPar + "</div>"+
+            "<div class='hcp box'>" + '' +"</div>"+
+            //"<div class='totalOut box' '+ p +'>" + '' +"</div>"+
+            "</div>"
+
+        );
+
+    createGolfCard();
+
 }
 
-
-function fillCard(){
+function createGolfCard(){
     for(let p = 1; p <= numPlayers; p++){
-        $(".left").append("<div class='playerLabel' contenteditable='true'>Player " + p + "</div>");
-        for(let h = 0; h < selCourse.data.holes.length; h++){
-            $("#c" + h).append("<input class='holeInput' id='p"+ p +"h"+ h +"' type='text'>");
+        $(".left").append(
+            "<div class='playerLabel playa golfer" + p + "'>" +
+            "<span onclick='deletePlayer(" + p + ")' class='far fa-trash-alt'></span>" +
+            "<span class='playerTag' contenteditable='true'>Player " + p + "</span>" +
+            "</div>");
+
+        $("#totalCol ").append("<div class='tot box playerTotal pTotal" + p + " playa golfer" + p + "'>0</div>");
+
+        for(let h = 0; h <= selCourse.data.holes.length; h++){
+            $("#c" + h).append("<input onkeyup='calcGolfScore(" + p + ")' class='holeInput playa golfer" + p + "' id='p"+ p +"h"+ h +"' type='text'>");
         }
     }
 }
 
-// function createCard(thetee){
-//     chosentee = thetee;
-//     $(".right").html('');
-//     $(".left").html('');
-//     for(let j = 0; j < course.holes.length; j++){
-//         $(".right").append("<div id='row"+ j +"' class='holeRow'><div class='rowHead'>"+ course.holes[j].name +"</div></div>");
-//     }
-//
-// }
-//
+amtOfHoles = 18;
+
+function calcGolfScore(myVal){
+    let tempScore = 0;
+    for(let i = 1; i <= amtOfHoles; i++){
+        tempScore += Number($("#p" + myVal + "h" + (i-1)).val());
+        //tempScore += inValue;
+    }
+    $(".pTotal" + myVal).html(tempScore);
+}
+
+function deletePlayer(playernum){
+    $(".golfer" + playernum).remove();
+    $(".total" + playernum).remove();
+    for(let i = 1; i <= amtOfHoles; i++){
+        $("#p" + playernum + "h" + i).remove();
+    }
+}
